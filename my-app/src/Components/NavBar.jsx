a6005813c4fe70afb1541d11f013dfbe511b1224
Shovan61 <shovonmazumder61@gmail.com>
2021-11-11 23:37:27 +0600

    added

--- my-app/src/Components/NavBar.jsx
+++ my-app/src/Components/NavBar.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import home from "../images/home-logo.svg";
 import search from "../images/search-icon.svg";
+import nHome from "../images/nav-home.svg";
+import jobs from "../images/nav-jobs.svg";
+import messaging from "../images/nav-messaging.svg";
+import network from "../images/nav-network.svg";
+import notifications from "../images/nav-notifications.svg";
+import work from "../images/nav-work.svg";
 
 function NavBar() {
   return (
@@ -11,6 +17,32 @@ function NavBar() {
           <img src={home} alt="home" />
           <input type="text" placeholder="Search" />
         </Search>
+        <NavItems>
+          <NavLink>
+            <img src={nHome} alt="" />
+            <span>Home</span>
+          </NavLink>
+          <NavLink>
+            <img src={network} alt="" />
+            <span>My Network</span>
+          </NavLink>
+          <NavLink>
+            <img src={jobs} alt="" />
+            <span>Jobs</span>
+          </NavLink>
+          <NavLink>
+            <img src={messaging} alt="" />
+            <span>Messaging</span>
+          </NavLink>
+          <NavLink>
+            <img src={notifications} alt="" />
+            <span>Notifications</span>
+          </NavLink>
+        </NavItems>
+
+        <Profile>
+          <img src="" alt="" />
+        </Profile>
       </Container>
     </Wrapper>
   );
@@ -21,21 +53,26 @@ export default NavBar;
 const Wrapper = styled.div`
   height: 6vh;
   background-color: white;
-  width: 100vw;
-  display: grid;
+  width: 100%;
+  display: flex;
   justify-content: center;
 `;
 
 const Container = styled.div`
-  /* max-width: 1300px; */
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
+  align-self: center;
+  max-width: 1300px;
+  width: 1280px;
+  height: 100%;
+  padding: 0 4rem;
+  /* border: 1px solid black; */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Search = styled.div`
-  grid-column-start: 1;
-  grid-column-end: 5;
-  border: 1px solid black;
+  width: 300px;
+  /* border: 1px solid black; */
   display: flex;
   align-items: center;
   height: 100%;
@@ -54,3 +91,31 @@ const Search = styled.div`
     }
   }
 `;
+
+const NavItems = styled.div`
+  width: 35%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const NavLink = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  & img {
+    height: 22px;
+    width: 22px;
+  }
+  & span {
+    font-size: 12px;
+    color: #444444;
+    margin-top: 0.2rem;
+  }
+`;
+
+const Profile = styled.div``;
